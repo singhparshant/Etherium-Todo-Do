dcbed1b4d029d903f074d11c0e1037885875a7ab
Parshant Singh <parshant.e2@gmail.com>
2021-07-25 23:03:39 +0530

    Etherium Todo List

---
+++ truffle-config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*", // Match any network id
+    },
+  },
+  solc: {
+    optimizer: {
+      enabled: true,
+      runs: 200,
+    },
+  },
+};
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*", // Match any network id
+    },
+  },
+  solc: {
+    optimizer: {
+      enabled: true,
+      runs: 200,
+    },
+  },
+};
